58e4bc871e1c4aa1bd3e40874f1c82e7f5584a03
Mariia-Marko <marko.work.t@gmail.com>
2021-03-24 11:12:08 +0300

    feat: add modal window

---
+++ source/script.js
@@ -0,0 +1,28 @@
+button.onclick = function () {
+    if (button.style.backgroundColor === 'black') {
+      button.style.backgroundColor = '#F87070';
+      button.style.color = 'black';
+    } else {
+    button.style.backgroundColor = '#F87070';
+      button.style.color = '#1E213F';
+      
+    }
+}
+
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('myBtn');
+let span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if ( event.target === modal ) {
+    modal.style.display = 'none';
+  }
+}
